ba73c9cc14bf4ef115156109e41b9291a0e10c32
ViktoryiaTamasheuskaya <39959716+ViktoryiaTamasheuskaya@users.noreply.github.com>
2018-06-29 15:17:48 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main (String[] args){
+
+        Group group= new Group();
+        group.createGroup();
+        group.avgGroup();
+        group.calcBadStudents();
+        group.calcGoodStudents();
+        group.printResults();
+    }
+}
